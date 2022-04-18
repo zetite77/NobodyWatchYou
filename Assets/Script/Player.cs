48496c285f74e0134e0b1dabacba2f1aba096ef0
zetite77 <zetite77@gmail.com>
2022-04-18 11:07:30 +0900

    플레이어 클래스 제작
    여러 스텟들 삽입

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float current_Hp;
+    private const float MAX_HP = 100.0f;
+    private float current_Stam;
+    private const float MAX_STAMINA = 100.0f;
+    private float Critical;
+    private float hungry;
+    private float Thirsty;
+    private float stealth;
+    private float weight;
+
+
+
+}
